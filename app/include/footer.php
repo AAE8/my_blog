02bf86a2267c7358bc0227a47a7db323dd56cfd6
ResearchActor <researchactor@gmail.com>
2023-03-26 17:32:11 +0300

    SYNC

---
+++ app/include/footer.php
@@ -0,0 +1,63 @@
+<div class="footer container-fluid">
+  <div class="footer-content container">
+    <div class="row">
+        
+      <div class="footer-section about col-md-4 col-12">
+        <h3 class="logo-text">Мой блог</h3>
+        <p>
+          Мой блог создан с целью разделения приобретенных в програмированнии знаний
+        </p>
+        <div class="contact">
+          <!-- <span><i class="fas fa-phone"></i> &nbsp; +7988-677-04-04</span> -->
+          <span><i class="fas fa-envelope"></i> &nbsp; researchactor@gmail.com</span>
+        </div>
+        <div class="socials">
+          <!-- <a href="#"><i class="fab fa-facebook"></i></a> -->
+          <!-- <a href="#"><i class="fab fa-instagram"></i></a> -->
+          <!-- <a href="#"><i class="fab fa-twitter"></i></a> -->
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </div>
+      </div>
+
+        <div class="footer-section links col-md-4 col-12">
+          <h3>Быстрые ссылки</h3>
+          <br>
+          <ul>
+            <a href="#">
+              <li>События</li>
+            </a>
+            <a href="#">
+              <li>Команда</li>
+            </a>
+            <a href="#">
+              <li>Упражнения</li>
+            </a>
+            <a href="#">
+              <li>Галерея</li>
+            </a>
+            <a href="#">
+              <li>Что-то еще...</li>
+            </a>
+          </ul>
+        </div>
+
+        <div class="footer-section contact-form col-md-4 col-12">
+          <h3>Контакты</h3>
+          <br>
+          <form action="/" method="post">
+            <input type="email" name="email" class="text-input contact-input" placeholder="Введите email...">
+            <textarea name="message" rows="4" class="text-input contact-input" placeholder="Введите сообщение..."></textarea>
+            <button type="submit" class="btn btn-big contact-btn">
+              <i class="fas fa-envelope"></i>
+              Отправить
+            </button>
+          </form>
+        </div>
+      
+      <div class="footer-bottom">
+        &copy; myblog.com | Designed by Adam
+      </div>
+      
+    </div>
+  </div>
+</div>
---
+++ reg.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <script src="https://kit.fontawesome.com/d68a41201f.js" crossorigin="anonymous"></script>
+    
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&family=Expletus+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Poiret+One&display=swap" rel="stylesheet">
+    <title>Регистрация</title>
+</head>
+<body>
+    
+<?php include("app/include/header.php"); ?>
+
+<!-- FORM -->
+<div class="container reg_form">
+<form class="row justify-content-md-center"  method="post" action="reg.php">
+    <h2>Форма регистрации</h2>
+    <div class="mb-3 col-md-4 col-12">
+        <label for="formGroupExampleInput" class="form-label">Введите логин</label>
+        <input type="text" class="form-control" id="formGroupExampleInput">
+    </div>
+    <div class="w-100"></div>
+    <div class="mb-3 col-md-4 col-12">
+      <label for="exampleInputEmail1" class="form-label">Введите email</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+    </div>
+    <div class="w-100"></div>
+    <div class="mb-3 col-md-4 col-12">
+      <label for="exampleInputPassword1" class="form-label">Введите пароль</label>
+      <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="w-100"></div>
+    <div class="mb-3 col-md-4 col-12">
+        <label for="exampleInputPassword2" class="form-label">Повторите пароль</label>
+        <input type="password" class="form-control" id="exampleInputPassword2">
+    </div>
+    <div class="w-100"></div>
+    <!-- <div class="mb-3 form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div> -->
+    <div class="mb-3 col-md-4 col-12">
+        <button type="button" class="btn btn-secondary">Отправить</button>
+        <a href="log.php">Авторизоваться</a>
+    </div>
+</form>
+</div>
+<!-- END FORM -->
+
+<!-- Блок FOOTER START-->
+<?php include("app/include/footer.php"); ?>
+  <!-- Блок FOOTER END-->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+  </html>
